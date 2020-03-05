6921dfebe401a9dee1710c8a3b3c2ed3b778ea73
hoya-1004 <whtjdgh104@naver.com>
2020-03-05 23:51:38 +0900

    modification

--- core/models.py
+++ core/models.py
@@ -4,3 +4,8 @@ class TimeStampedModel(models.Model):
 
     """Time Stamped Model"""
 
+    created = models.DateTimeField(auto_now_add = True) 
+    updated = models.DateTimeField(auto_now = True)    
+
+    class Meta:
+        abstract = True
