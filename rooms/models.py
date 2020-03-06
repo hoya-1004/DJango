e9c059271864bf0dd0a091ff0c82961c74b4815a
hoya-1004 <whtjdgh104@naver.com>
2020-03-06 23:56:12 +0900

    modification

--- rooms/models.py
+++ rooms/models.py
@@ -3,6 +3,43 @@ from django_countries.fields import CountryField
 from core import models as core_models
 from users import models as user_models
 
+class AbstractItem(core_models.TimeStampedModel):
+    
+    """ Abstract Item """
+
+    name = models.CharField(max_length = 80)
+
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+class RoomType(AbstractItem):
+    
+    """ RoomType Model Definition """
+
+    pass
+
+class Amenity(AbstractItem):
+    
+    """ Amenity Model Definition """
+
+    pass
+
+class Facility(AbstractItem):
+
+    """Facility Model Definition"""
+
+    pass
+
+class HouseRule(AbstractItem):
+
+    """HouseRule Model Definition"""
+
+    pass
+
 class Room(core_models.TimeStampedModel):
     
     """Room Model Definition"""
@@ -21,3 +58,11 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default = False)
     host = models.ForeignKey(user_models.User, on_delete = models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete = models.SET_NULL, null = True)
+    amenities = models.ManyToManyField(Amenity)
+    facility = models.ManyToManyField(Faciltiy)
+    house_rule = models.ManyToManyField(HouseRule)
+
+
+    def __str__(self):
+        return self.name
