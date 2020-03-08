ff04ce63a20e970d57aef698074e8e170e9ad688
hoya-1004 <whtjdgh104@naver.com>
2020-03-08 21:52:57 +0900

    modification

--- rooms/models.py
+++ rooms/models.py
@@ -1,33 +1,36 @@
-from django.db import models 
+from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
 
+
 class AbstractItem(core_models.TimeStampedModel):
-    
+
     """ Abstract Item """
 
-    name = models.CharField(max_length = 80)
+    name = models.CharField(max_length=80)
 
-
-    class Meta:  
+    class Meta:
         abstract = True
 
     def __str__(self):
         return self.name
 
+
 class RoomType(AbstractItem):
-    
+
     """ RoomType Model Definition """
 
     class Meta:
         verbose_name = "Room Tpye"
 
+
 class Amenity(AbstractItem):
-    
+
     """ Amenity Model Definition """
 
     class Meta:
         verbose_name_plural = "Amenities"
+
 
 class Facility(AbstractItem):
 
@@ -36,6 +39,7 @@ class Facility(AbstractItem):
     class Meta:
         verbose_name_plural = "Facilities"
 
+
 class HouseRule(AbstractItem):
 
     """HouseRule Model Definition"""
@@ -43,40 +47,41 @@ class HouseRule(AbstractItem):
     class Meta:
         verbose_name = "House Rule"
 
+
 class Photo(core_models.TimeStampedModel):
 
     """ Photo Model Definition """
 
-    caption = models.CharField(max_length = 80)
-    file =models.ImageField()
-    room = models.ForeignKey("Room", on_delete = models.CASCADE)
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption
 
+
 class Room(core_models.TimeStampedModel):
-    
+
     """Room Model Definition"""
 
-    name = models.CharField(max_length = 140) 
-    description = models.TextField()   
+    name = models.CharField(max_length=140)
+    description = models.TextField()
     country = CountryField()
-    city = models.CharField(max_length = 80)
+    city = models.CharField(max_length=80)
     price = models.IntegerField()
-    address = models.CharField(max_length = 140)
+    address = models.CharField(max_length=140)
     guests = models.IntegerField()
     beds = models.IntegerField()
     bedrooms = models.IntegerField()
     baths = models.IntegerField()
     check_in = models.TimeField()
     check_out = models.TimeField()
-    instant_book = models.BooleanField(default = False)
-    host = models.ForeignKey("users.User", on_delete = models.CASCADE)
-    room_type = models.ForeignKey("RoomType", on_delete = models.SET_NULL, null = True)
-    amenities = models.ManyToManyField("Amenity", blank = True)
-    facility = models.ManyToManyField("Facility", blank = True)
-    house_rule = models.ManyToManyField("HouseRule", blank = True)
-
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facility = models.ManyToManyField("Facility", blank=True)
+    house_rule = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
 
     """Custom User Model"""
@@ -18,24 +19,17 @@ class User(AbstractUser):
     LANGUAGE_ENGLISH = "en"
     LANGUAGE_KOREAN = "kr"
 
-    LANGUAGE_CHOICES = (
-        (LANGUAGE_ENGLISH, "English"), 
-        (LANGUAGE_KOREAN, "Korean")
-        )
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
 
     CURRENCY_USD = "usd"
     CURRENCY_USD = "krw"
 
-    CURRENCY_CHOICES = (
-        (CURRENCY_USD, "USD"),
-         (CURRENCY_USD, "KRW")
-        )
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_USD, "KRW"))
 
-
-    avatar = models.ImageField(blank = True)
-    gender = models.CharField(choices = GENDER_CHOICES, max_length=10, blank = True)    
-    bio = models.TextField(blank = True)
-    birthdate = models.DateField(blank = True, null = True)
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICES, max_length=10, blank=True)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
     language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
     currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
     superhost = models.BooleanField(default=False)
