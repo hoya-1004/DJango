2be0cd816b2532c8db8059a51b81adec5340eaa8
hoya-1004 <whtjdgh104@naver.com>
2020-03-09 23:58:44 +0900

    modification

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,59 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition"""
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "conutry", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")},),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "paths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facility", "house_rule"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facility",
+        "house_rule",
+        "city",
+        "country",
+    )
+
+    search_fields = (
+        "=city",
+        "^host__username",
+    )
+
+    filter_horizontal = (
+        "amenities",
+        "facility",
+        "house_rule",
+    )
 
 
 @admin.register(models.Photo)
@@ -24,4 +76,3 @@ class PhotoAdmin(admin.ModelAdmin):
     """ """
 
     pass
-
